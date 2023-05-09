0dd7e45a3f610879f5411ec017eb7e18486f87da
juhodufva <juho.dufva@student.lab.fi>
2023-05-09 17:36:47 +0300

    suve

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+
 import { MeistaComponent } from './meista/meista.component';
+import { EtusivuComponent } from './etusivu/etusivu.component';
+import { VertaileComponent } from './vertaile/vertaile.component';
+import { RekisteroidyComponent } from './rekisteroidy/rekisteroidy.component';
+import { LaskinComponent } from './laskin/laskin.component';
+
 
 const routes: Routes = [
-  {path:'', component:AppComponent},
-  {path:'meista', component:MeistaComponent},
+  {path: '', component: EtusivuComponent},
+  {path: 'meista', component: MeistaComponent},
+  {path: 'etusivu', component: EtusivuComponent},
+  {path: 'vertaile', component: VertaileComponent},
+  {path: 'rekisteroidy', component: RekisteroidyComponent},
+  {path: 'laskin', component: LaskinComponent}
 ];
 
 @NgModule({
